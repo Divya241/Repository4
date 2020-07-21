e8bc6249631704c6aa3c742cc90504174a35c60e
Divya241 <divyakrishnamoorthy24@gmail.com>
2020-07-21 12:37:53 +0530

    Code Added

--- src/test/java/org/samp/Task4.java
+++ src/test/java/org/samp/Task4.java
@@ -1,7 +1,11 @@
 package org.samp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task4 {
 public static void main(String[] args) {
-	
+	WebDriver driver=new ChromeDriver();
+
 }
 }
